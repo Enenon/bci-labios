23cebcea41c192f473b9bd261bd0edb80c19cc4d
Enenon <98350328+Enenon@users.noreply.github.com>
2025-12-09 17:38:40 -0300

    Adicionado FFT plot
    - Adicionei o FFT da serie temporal;
    - Tirei a redundância dos imports dos widgets;
    - Otimizei o código colocando a opção de rodá-lo sem importar a biblioteca Tensorflw.

--- gui/gui.py
+++ gui/gui.py
@@ -1,24 +1,26 @@
-from keras.models import load_model
-from tensorflow.keras.optimizers import Adam
-import os
-
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-
-from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QVBoxLayout, QLabel, QSizePolicy
+from PyQt5 import QtCore, QtGui
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QVBoxLayout, QLabel, QSizePolicy
+from PyQt5.QtWidgets import * 
 import sys
 import numpy as np
+from scipy.fft import fft
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
 import numpy as np
-import keyboard  # para detectar ESC
-import time
 from pylsl import StreamInlet, resolve_stream, resolve_byprop
 from time import sleep
 import matplotlib.pyplot as plt
 from random import choice
-import mne
+
+usar_modelo = False
+if usar_modelo:
+    from keras.models import load_model
+    from tensorflow.keras.optimizers import Adam
+    import os
+    os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+
 
 class JanelaInicial(QMainWindow):
     def __init__(self):
@@ -33,46 +35,46 @@ class JanelaInicial(QMainWindow):
         self.limiar = 0.4
 
         # Setup da UI com layouts (coluna esquerda = controles, coluna direita = visualização expansível)
-        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget = QWidget(self)
         self.setCentralWidget(self.centralwidget)
-        self.main_layout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.main_layout = QHBoxLayout(self.centralwidget)
         self.main_layout.setContentsMargins(8, 8, 8, 8)
 
         # coluna esquerda (controles) - não expande
-        self.left_widget = QtWidgets.QWidget(self.centralwidget)
-        self.left_layout = QtWidgets.QVBoxLayout(self.left_widget)
+        self.left_widget = QWidget(self.centralwidget)
+        self.left_layout = QVBoxLayout(self.left_widget)
         self.left_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.addWidget(self.left_widget, 0)
 
         # coluna direita (visualização) - expande com a janela
-        self.visual_widget = QtWidgets.QWidget(self.centralwidget)
-        self.Layout_visualizacao = QtWidgets.QVBoxLayout(self.visual_widget)
+        self.visual_widget = QWidget(self.centralwidget)
+        self.Layout_visualizacao = QVBoxLayout(self.visual_widget)
         self.Layout_visualizacao.setContentsMargins(0, 0, 0, 0)
         self.main_layout.addWidget(self.visual_widget, 1)
  
         # --- CONFIGURAÇÃO OTIMIZADA DO MATPLOTLIB ---
-        self.frameGrafico = QtWidgets.QFrame(self.visual_widget)
-        self.frameGrafico_layout = QtWidgets.QVBoxLayout(self.frameGrafico)
+        self.frameGrafico = QFrame(self.visual_widget)
+        self.frameGrafico_layout = QVBoxLayout(self.frameGrafico)
         
         # Criamos a figura uma única vez
         self.figure = Figure(figsize=(5, 3), dpi=100)
         self.canvas = FigureCanvas(self.figure)
-        self.ax = self.figure.add_subplot(111)
+        self.ax_seriet = self.figure.add_subplot(111)
         
         # Configuração estética do gráfico
-        self.ax.set_title('Série Temporal EEG (Real-time)')
-        self.ax.set_xlabel('Amostras')
-        self.ax.set_yticks([]) # Remove eixo Y numérico para limpar
-        self.ax.set_xlim(0, self.epochsize)
+        self.ax_seriet.set_title('Série Temporal EEG (Real-time)')
+        self.ax_seriet.set_xlabel('Amostras')
+        self.ax_seriet.set_yticks([]) # Remove eixo Y numérico para limpar
+        self.ax_seriet.set_xlim(0, self.epochsize)
         # Ajusta limite Y para caber todos os canais empilhados (waterfall)
         self.escala_visual = 750 # Fator para separar as linhas visualmente
-        self.ax.set_ylim(-self.escala_visual, self.n_channels * self.escala_visual + self.escala_visual)
+        self.ax_seriet.set_ylim(-self.escala_visual, self.n_channels * self.escala_visual + self.escala_visual - 700) #botei esse 700 pra ficar mais encaixado no grafico
         
         # CRUCIAL: Criamos as linhas (artistas) vazias agora e guardamos as referências
         self.lines = []
         for i in range(self.n_channels):
            # Plotamos uma linha vazia para cada canal
-           line, = self.ax.plot([], [], lw=1) 
+           line, = self.ax_seriet.plot([0,self.epochsize], 2*[i*self.escala_visual], lw=1) 
            self.lines.append(line)
 
         self.frameGrafico_layout.addWidget(self.canvas)
@@ -81,51 +83,60 @@ class JanelaInicial(QMainWindow):
         self.frameGrafico.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.Layout_visualizacao.addWidget(self.frameGrafico)
  
-        self.ax.set_title('Série Temporal EEG')
-        self.ax.set_xlabel('Tempo (s)')
-        self.frameGrafico.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frameGrafico.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.ax_seriet.set_title('Série Temporal EEG')
+        self.ax_seriet.set_xlabel('Tempo (s)')
+        self.frameGrafico.setFrameShape(QFrame.StyledPanel)
+        self.frameGrafico.setFrameShadow(QFrame.Raised)
         self.frameGrafico.setObjectName("frameGrafico")
  
         # área de saída abaixo do gráfico
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.saida = QtWidgets.QLabel('Saída:', self.visual_widget)
+        self.comboBoxPlots = QComboBox(self.visual_widget)
+        self.comboBoxPlots.addItem('Série temporal')
+        self.comboBoxPlots.addItem('FFT')
+        self.horizontalLayout.addWidget(self.comboBoxPlots)
+        self.button_abrirjanela = QPushButton('Abir em nova janela',self.visual_widget)
+        self.horizontalLayout.addWidget(self.button_abrirjanela)
+        self.saida = QLabel('Saída:', self.visual_widget)
         self.saida.setObjectName("saida")
         self.horizontalLayout.addWidget(self.saida)
-        self.label_4 = QtWidgets.QLabel('0', self.visual_widget)
-        self.label_4.setObjectName("label_4")
-        self.horizontalLayout.addWidget(self.label_4)
+        self.label_previsao = QLabel('0', self.visual_widget)
+        self.label_previsao.setObjectName("label_previsao")
+        
+        self.button_abrirjanela.clicked.connect(self.abrir_janela_plot)
+        self.horizontalLayout.addWidget(self.label_previsao)
+        
         self.Layout_visualizacao.addLayout(self.horizontalLayout)
         
         # aqui ficará a imagem do cérebro
-        self.frameCerebro = QtWidgets.QFrame(self.visual_widget)
-        self.frameCerebro.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frameCerebro.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frameCerebro = QFrame(self.visual_widget)
+        self.frameCerebro.setFrameShape(QFrame.StyledPanel)
+        self.frameCerebro.setFrameShadow(QFrame.Raised)
         self.frameCerebro.setObjectName("frameCerebro")
         self.Layout_visualizacao.addWidget(self.frameCerebro)
  
         # Widget com status (na coluna esquerda)
-        self.formLayoutWidget = QtWidgets.QWidget(self.left_widget)
-        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.formLayoutWidget = QWidget(self.left_widget)
+        self.formLayout = QFormLayout(self.formLayoutWidget)
         self.formLayout.setContentsMargins(0, 0, 0, 0)
-        self.label_3 = QtWidgets.QLabel('Status do modelo: ', self.formLayoutWidget)
-        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_3)
-        self.label_5 = QtWidgets.QLabel('Nenhum modelo carregado', self.formLayoutWidget)
-        self.label_5.setWordWrap(True)
-        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.label_5)
-        self.label = QtWidgets.QLabel('Status do LSL: ', self.formLayoutWidget)
-        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label)
-        self.label_2 = QtWidgets.QLabel('Desconectado', self.formLayoutWidget)
-        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.label_2)
+        self.label_status_modelo = QLabel('Status do modelo: ', self.formLayoutWidget)
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label_status_modelo)
+        self.modelo_infos = QLabel('Nenhum modelo carregado', self.formLayoutWidget)
+        self.modelo_infos.setWordWrap(True)
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.modelo_infos)
+        self.label_status_lsl = QLabel('Status do LSL: ', self.formLayoutWidget)
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_status_lsl)
+        self.status_lsl = QLabel('Desconectado', self.formLayoutWidget)
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.status_lsl)
         self.left_layout.addWidget(self.formLayoutWidget)
  
         # botões (embaixo do form na coluna esquerda)
-        self.buttonLayoutWidget = QtWidgets.QWidget(self.left_widget)
-        self.buttonLayout = QtWidgets.QVBoxLayout(self.buttonLayoutWidget)
-        self.button = QtWidgets.QPushButton('Conectar LSL', self.buttonLayoutWidget)
+        self.buttonLayoutWidget = QWidget(self.left_widget)
+        self.buttonLayout = QVBoxLayout(self.buttonLayoutWidget)
+        self.button = QPushButton('Conectar LSL', self.buttonLayoutWidget)
         self.buttonLayout.addWidget(self.button)
-        self.button_iniciarBCI = QtWidgets.QPushButton('Iniciar BCI', self.buttonLayoutWidget)
+        self.button_iniciarBCI = QPushButton('Iniciar BCI', self.buttonLayoutWidget)
         self.buttonLayout.addWidget(self.button_iniciarBCI)
         self.left_layout.addWidget(self.buttonLayoutWidget)
         self.left_layout.addStretch()
@@ -144,10 +155,10 @@ class JanelaInicial(QMainWindow):
         self.palette_verde = QtGui.QPalette()
         brush_verde = QtGui.QBrush(QtGui.QColor(4, 150, 0))
         self.palette_verde.setBrush(QtGui.QPalette.All, QtGui.QPalette.WindowText, brush_verde)
-        self.label_2.setPalette(self.palette_vermelha)
+        self.status_lsl.setPalette(self.palette_vermelha)
 
         
-        self.setStatusBar(QtWidgets.QStatusBar(self))
+        self.setStatusBar(QStatusBar(self))
 
         self.menubar = self.menuBar()
 
@@ -163,7 +174,33 @@ class JanelaInicial(QMainWindow):
         self.button_iniciarBCI.clicked.connect(self.iniciar_bci)
         QtCore.QMetaObject.connectSlotsByName(self)
         #self.show()
+
+        # O plot da FFT é idêntico ao da série temporal
+
+        self.figure_FFT = Figure(figsize=(5, 3), dpi=100)
+        self.canvas_FFT = FigureCanvas(self.figure_FFT)
+
+        self.ax_fft = self.figure_FFT.add_subplot(111) # subplot com 1 linha e 1 coluna no índice 1
+        
+        self.escala_FFT = 200
+        self.normalizacaoFFT = 1
+        self.ax_fft.set_title('Transformada de Fourier')
+        self.ax_fft.set_xlabel('Frequencia')
+        self.ax_fft.set_yticks([])
+        self.ax_fft.set_xlim(0, self.epochsize)
+
+        self.ax_fft.set_ylim(-self.escala_visual, self.n_channels * self.escala_FFT + self.escala_FFT - 700) #botei esse 700 pra ficar mais encaixado no grafico
+        
+
+        self.lines_fft = []
+        for i in range(self.n_channels):
+           # Plotamos uma linha vazia para cada canal
+           line_fft, = self.ax_fft.plot([0,self.epochsize], 2*[i*self.escala_FFT], lw=1) 
+           self.lines_fft.append(line_fft)
+
+        
     
+
 
 
     def iniciar_bci(self):
@@ -183,9 +220,6 @@ class JanelaInicial(QMainWindow):
         if not chunk:
            return 
 
-
-
-
         # 2. Atualização do Buffer Circular (Numpy é mais rápido que lista append/pop)
         # Desloca os dados antigos para a esquerda e insere os novos no final
         new_len = len(chunk)
@@ -202,70 +236,132 @@ class JanelaInicial(QMainWindow):
         except:
            pred = 'Err'
 
-        self.label_4.setText(str(pred))
+        self.label_previsao.setText(str(pred))
 
         # 4. ATUALIZAÇÃO DO GRÁFICO (A mágica acontece aqui)
         # Eixo X é sempre o mesmo (0 a epochsize)
         x_data = np.arange(self.epochsize)
         
         for i, line in enumerate(self.lines):
-           # Pegamos os dados do canal i
-           channel_data = self.current_data[:, i]
+            # Pegamos os dados do canal i
+            channel_data = self.current_data[:, i]
+            
+            # Adicionamos um offset para criar o efeito "Waterfall" (um canal em cima do outro)
+            # Sem isso, todos os 16 canais ficariam misturados no zero.
+            offset = i * self.escala_visual
            
-           # Adicionamos um offset para criar o efeito "Waterfall" (um canal em cima do outro)
-           # Sem isso, todos os 16 canais ficariam misturados no zero.
-           offset = i * self.escala_visual
-           
-           line.set_data(x_data, channel_data + offset)
+            line.set_data(x_data, channel_data + offset)
+
+            fft_data = fft(channel_data)/self.normalizacaoFFT
+            self.lines_fft[i].set_data([i for i in range(len(fft_data))],fft_data + i * self.escala_FFT)
 
         # Redesenha apenas o canvas
         self.canvas.draw_idle()
+
+        self.canvas_FFT.draw_idle()
+
 
 
     def abrir_janela_teste(self):   
         self.janela_teste = JanelaTeste()
 
     def abrir_modelo(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', 
-   '../',"Model files (*.h5)")
-        print(fname)
-        try:
-           self.model = load_model(fname[0])
-           self.model.compile(optimizer=Adam(1e-4), loss='binary_crossentropy', metrics=['accuracy'])
-           self.label_5.setText('shape:'+str(self.model.input_shape)+'\noutput shape:'+str(self.model.output_shape)+'\nmetric names:'+
-                              str(self.model.metrics_names))
-           self.label_5.setPalette(self.palette_verde)
-        except:
-           self.label_5.setText('Modelo incompatível.')
-           self.label_5.setPalette(self.palette_vermelha)
+        if usar_modelo:
+            fname = QFileDialog.getOpenFileName(self, 'Open file', 
+    '../',"Model files (*.h5)")
+            print(fname)
+            try:
+                self.model = load_model(fname[0])
+                self.model.compile(optimizer=Adam(1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+                self.modelo_infos.setText('shape:'+str(self.model.input_shape)+'\noutput shape:'+str(self.model.output_shape)+'\nmetric names:'+
+                                    str(self.model.metrics_names))
+                self.modelo_infos.setPalette(self.palette_verde)
+            except:
+                self.modelo_infos.setText('Modelo incompatível.')
+                self.modelo_infos.setPalette(self.palette_vermelha)
+        else: return
         #model.summary()
 
     def conectar_LSL(self):
         print("Aguardando stream EEG...")
         print('mudando a cor da paleta')
 
-        self.label_2.setText('Procurando...')
-        self.label_2.setPalette(self.palette_amarela)
+        self.status_lsl.setText('Procurando...')
+        self.status_lsl.setPalette(self.palette_amarela)
         QApplication.processEvents() # <--- isso aplica as mudanças antes da função acabar
         self.streams = resolve_byprop('type', 'EEG',timeout=3)
         if self.streams:
            self.inlet = StreamInlet(self.streams[0])
            palette = QtGui.QPalette()
            palette.setBrush(QtGui.QPalette.All, QtGui.QPalette.WindowText,QtGui.QBrush(QtGui.QColor(4,150,0)))
-           self.label_2.setText('Conectado!')
-           self.label_2.setPalette(self.palette_verde)
+           self.status_lsl.setText('Conectado!')
+           self.status_lsl.setPalette(self.palette_verde)
         else:
            print('Não achou conexão')
-           self.label_2.setPalette(self.palette_vermelha)
-           self.label_2.setText('Desconectado')
+           self.status_lsl.setPalette(self.palette_vermelha)
+           self.status_lsl.setText('Desconectado')
+
+    def abrir_janela_plot(self):
+        widget_escolhido = self.comboBoxPlots.currentIndex()
+
+        self.janela_plot = JanelaPlot(self,widget_escolhido)
+
+
+    def update_FFT(self):
+        self.t += self.timer.interval()/100
+        self.ax_FFT.clear()
+        self.ax_FFT.plot(self.t,np.sin(self.t))
+        self.canvas_FFT.draw_idle()
            
+class JanelaPlot(QMainWindow):
+    def __init__(self,principal,index):
+        super().__init__()
+        self.janelaPrincipal = principal
+        self.setGeometry(200,200,350,350)
+        self.setWindowTitle('Plot')
+        self.centralwidget = QWidget(self)
+        self.setCentralWidget(self.centralwidget)
+        self.figure = Figure(figsize=(5, 3), dpi=100)
+        self.canvas = FigureCanvas(Figure(figsize=(5,4)))
+        self.layout1 = QVBoxLayout(self.centralwidget)
+        match index:
+            case 0:
+                #self.canvas = principal.canvas
+                self.layout1.addWidget(principal.canvas)
+            case 1:
+                #self.canvas = principal.canvas_FFT
+                self.layout1.addWidget(principal.canvas_FFT,99)
+                self.normtext = QPlainTextEdit(self.centralwidget)
+                #self.layout1.addWidget(self.normtext,1)
+
+                self.layoutConfigs = QHBoxLayout(self.centralwidget)
+                self.layout1.addLayout(self.layoutConfigs,1)
+                self.normtext.setPlainText(str(principal.normalizacaoFFT))
+                self.normButton = QPushButton('Normalizar',self.centralwidget)
+                self.layoutConfigs.addWidget(self.normtext,1)
+                self.layoutConfigs.addWidget(self.normButton,1)
+                self.layoutConfigs.addWidget(QWidget(self),8)
+
+                self.normButton.clicked.connect(self.normalizar)
+
+        
+        self.show()
+
+    def normalizar(self):
+        num = self.normtext.toPlainText()
+        print(num)
+        self.janelaPrincipal.normalizacaoFFT = float(num)
+
+
+
+
 
 class JanelaTeste(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(200,200,350,50)
         self.setWindowTitle('Teste')
-        self.label = QtWidgets.QLabel("Recurso indisponível!",self)
+        self.label = QLabel("Recurso indisponível!",self)
         self.label.setGeometry(QtCore.QRect(1,8,100,23))
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.mudar_cor)
@@ -293,7 +389,7 @@ class Janela1(QMainWindow):
         self.setGeometry(200,200,300,300)
         self.setWindowTitle('Janela 1')
 
-        self.button = QtWidgets.QPushButton('Clique aqui',self)
+        self.button = QPushButton('Clique aqui',self)
         self.button.move(50,50)
         self.button.clicked.connect(self.open_new_window)
         self.show()
@@ -307,9 +403,9 @@ class Janela2(QMainWindow):
         self.setGeometry(300,300,350,350)
         self.setWindowTitle('Janela 2')
 
-        widget1 = QtWidgets.QWidget()
+        widget1 = QWidget()
         self.setCentralWidget(widget1)
-        layout = QtWidgets.QVBoxLayout(widget1)
+        layout = QVBoxLayout(widget1)
 
         self.canvas = FigureCanvas(Figure(figsize=(5,4)))
         layout.addWidget(self.canvas)
